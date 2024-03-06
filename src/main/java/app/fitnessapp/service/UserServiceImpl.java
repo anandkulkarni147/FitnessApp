2908e790b505870f96e1e38faa6f90bb0c9e5724
Anand Kulkarni <kulkarnianand147@gmail.com>
2024-03-05 18:05:58 -0800

    user service layer

--- src/main/java/app/fitnessapp/service/UserServiceImpl.java
+++ src/main/java/app/fitnessapp/service/UserServiceImpl.java
@@ -4,18 +4,12 @@ import app.fitnessapp.model.User;
 import app.fitnessapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import javax.transaction.Transactional;
-import org.springframework.mail.javamail.JavaMailSender;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @Autowired
-    private JavaMailSender javaMailSender;
 
     @Override
     public User getUserById(Long userId) {
